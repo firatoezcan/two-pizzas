3fecc5dbf9d0a1cf54579983d4242f8d25e3bacc
Firat Özcan <firat.oezcan@gmail.com>
2020-12-11 15:28:48 +0100

    Initial commit: WMR and Tailwind

---
+++ postcss.config.js
@@ -0,0 +1,24 @@
+const plugins = {
+  tailwindcss: {},
+  autoprefixer: {},
+
+}
+
+const productionPlugins = {
+  "@fullhuman/postcss-purgecss": {
+    content: [
+      './public/pages/**/*.{ts,tsx}',
+      './public/components/**/*.{ts,tsx}',
+    ],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+  cssnano: {}
+}
+
+if (process.env.NODE_ENV === "production") {
+  Object.entries(productionPlugins).forEach(([plugin, config]) => plugins[plugin] = config)
+}
+
+module.exports = {
+  plugins
+}
---
+++ public/index.tsx
@@ -0,0 +1,32 @@
+import hydrate from "preact-iso/hydrate";
+import { LocationProvider, Router } from "preact-iso/router";
+import lazy, { ErrorBoundary } from "preact-iso/lazy";
+import Home from "./pages/home/index";
+import NotFound from "./pages/_404";
+import Header from "./header";
+
+const About = lazy(() => import("./pages/about/index"));
+
+export function App() {
+  return (
+    <LocationProvider>
+      <div class="app">
+        <Header />
+        <ErrorBoundary>
+          <Router>
+            <Home path="/" />
+            <About path="/about" />
+            <NotFound default />
+          </Router>
+        </ErrorBoundary>
+      </div>
+    </LocationProvider>
+  );
+}
+
+hydrate(<App />);
+
+export async function prerender(data) {
+  const { default: prerender } = await import("preact-iso/prerender");
+  return await prerender(<App {...data} />);
+}
